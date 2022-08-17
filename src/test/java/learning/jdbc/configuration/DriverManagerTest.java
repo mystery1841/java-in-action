76be4b3a623b187c9e3e89e11d44b98fe8a2df60
myste <mystory1840@outlook.com>
2022-08-17 17:24:05 +0800

    add Apache Derby

---
+++ src/test/java/learning/jdbc/configuration/DriverManagerTest.java
@@ -0,0 +1,37 @@
+package learning.jdbc.configuration;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.Properties;
+
+public class DriverManagerTest {
+
+    @Test
+    public void testConnectionDatabase() throws IOException, SQLException {
+        Properties props = new Properties();
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream("database.properties")) {
+            props.load(in);
+        }
+        String drivers = props.getProperty("jdbc.drivers");
+        if (drivers != null)
+            System.setProperty("jdbc.drivers", drivers);
+        String url = props.getProperty("jdbc.url");
+        String username = props.getProperty("jdbc.username");
+        String password = props.getProperty("jdbc.password");
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             Statement stat = conn.createStatement()) {
+            stat.executeUpdate("CREATE TABLE Greetings (Message CHAR(20))");
+            stat.executeUpdate("INSERT INTO Greetings VALUES ('Hello, World!')");
+            try (ResultSet result = stat.executeQuery("SELECT * FROM Greetings")) {
+                if (result.next())
+                    System.out.println(result.getString(1));
+            }
+            stat.executeUpdate("DROP TABLE Greetings");
+        }
+    }
+}
